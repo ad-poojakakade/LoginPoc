7434d455e3aa1593012a9ede803c4e0444a188e0
Pooja Kakade <pooja.kakade@asl.mum.sst>
2026-03-06 18:05:42 +0530

    Added environment dropdown to GitHub workflow

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 import { env } from '../config/env';
 
-test.only('Login form automate', async ({ page }) => {
+test('Login form automate', async ({ page }) => {
 
   const loginPage = new LoginPage(page);
 
@@ -10,22 +10,3 @@ test.only('Login form automate', async ({ page }) => {
   await loginPage.login(env.username!, env.password!);
 
 });
-// import {chromium,test} from "@playwright/test"
-
-// test("Login form automate", async()=>{
-
-//     const browser= await chromium.launch({
-//       //  headless:false
-//     })
-
-//   const context = await browser.newContext()  
-//   const page = await context.newPage()
-
-// await page.goto("https://www.facebook.com/")
-
-// await page.fill("input[id='email']","pkakade@yopmail.com")
-// await page.fill("input[id='pass']","Savana1#")
-
-// await page.click("'Log in'")
-
-// })
